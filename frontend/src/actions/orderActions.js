12e62ea7a72bd73681429cc57cfe39ebf68abe76
neerajmishra25 <35920629+neerajmishra25@users.noreply.github.com>
2021-03-19 01:29:08 +0530

    pay order reducer and action added

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -5,6 +5,10 @@ import {
 	ORDER_DETAILS_REQUEST,
 	ORDER_DETAILS_SUCCESS,
 	ORDER_DETAILS_FAILED,
+	ORDER_PAY_REQUEST,
+	ORDER_PAY_SUCCESS,
+	ORDER_PAY_FAILED,
+	ORDER_PAY_RESET,
 } from "../constants/orderConstants";
 import axios from "axios";
 
@@ -50,6 +54,35 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
 	} catch (error) {
 		dispatch({
 			type: ORDER_DETAILS_FAILED,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+
+export const payOrder = (id, paymentResult) => async (dispatch, getState) => {
+	try {
+		dispatch({ type: ORDER_PAY_REQUEST });
+		const {
+			userLogin: { userInfo },
+		} = getState();
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		};
+		const { data } = await axios.put(
+			`/api/orders/${id}/pay`,
+			paymentResult,
+			config
+		);
+		dispatch({ type: ORDER_PAY_SUCCESS, payload: data });
+	} catch (error) {
+		dispatch({
+			type: ORDER_PAY_FAILED,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
