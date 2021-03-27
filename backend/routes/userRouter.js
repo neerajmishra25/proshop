d0e01effbcd1cf1826c8c58c0f069175cd35dda1
neerajmishra25 <35920629+neerajmishra25@users.noreply.github.com>
2021-03-27 23:29:54 +0530

    get single and update single user api added

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -9,5 +9,7 @@ router.get("/profile", protect, userController.getUserProfile);
 router.get("/", protect, admin, userController.getAllUsers);
 router.put("/profile", protect, userController.updateUserProfile);
 router.delete("/:id", protect, admin, userController.deleteUser);
+router.get("/:id", protect, admin, userController.getUserById);
+router.put("/:id", protect, admin, userController.updateUser);
 
 module.exports = router;
